47956f0173b259fc1240b80525eb1173028042c0
ConsolAktif <sigortataciri@gmail.com>
2025-01-06 14:45:23 +0300

    prop & array mapping.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,36 @@ import Input from "./components/Input";
 import User from "./components/User";
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Murat");
+  const friends = [
+    {
+      id: 1,
+      name: "Murat",
+    },
+    {
+      id: 2,
+      name: "Ahmet",
+    },
+    {
+      id: 3,
+      name: "Tayfur",
+    },
+    {
+      id: 4,
+      name: "Kratos",
+    },
+  ];
 
   return (
     <>
       <h2>{`Hello my name is ${name}`}</h2>
       <Header />
       <Input setName={setName} />
-      <User name={Murat} isLoggedIn={false} />
+      <User
+        name={name}
+        isLoggedIn={true}
+        friends={friends}
+      />
     </>
   );
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const User = ({ name, isLoggedIn }) => {
-  return <>{isLoggedIn ? <div>{name}</div> : <p>Lütfen kayıt olunuz!</p>}</>;
+const User = ({ name, isLoggedIn, friends }) => {
+  return (
+    <>
+      {isLoggedIn ? (
+        <div>
+          <p>{name}</p>
+          <h2>Arkadaşlarım</h2>
+          {friends.map((friend) => (
+            <div key={friend.id}>
+              {friend.id} - {friend.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Lütfen kayıt olunuz!</p>
+      )}
+    </>
+  );
 };
 
 export default User;
